e147b0f2709a45aa47cb2e152a6e8d0e3147c36d
Hesam760 <ghasemi.hesam3@gmail.com>
2022-01-23 10:54:45 +0330

    add index method in rope class

--- src/Program.java
+++ src/Program.java
@@ -1,43 +1,47 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Program {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
-            ArrayList<Rope> ropes = new ArrayList<Rope>();
+            ArrayList<Rope> ropes = new ArrayList<>();
             String input = in.readLine();
             StringTokenizer st = new StringTokenizer(input);
             String check = st.nextToken();
             if (check.equalsIgnoreCase("new")) {
-                String pass = input.substring(5,input.length()-1);
+                String pass = input.substring(5, input.length() - 1);
                 Rope newRope = new Rope(pass);
                 newRope.print(newRope.getRoot());
                 ropes.add(newRope);
-            }
-            else if (check.equalsIgnoreCase("print")) {
+            } else if (check.equalsIgnoreCase("print")) {
                 Rope print = ropes.get(0);
                 print.print(print.getRoot());
-            }
-            else if (check.equalsIgnoreCase("concat")) {
+            } else if (check.equalsIgnoreCase("concat")) {
                 int first = Integer.parseInt(st.nextToken());
                 int second = Integer.parseInt(st.nextToken());
-                Rope firstRope = ropes.get(first-1);
-                Rope secondRope = ropes.get(second-1);
+                Rope firstRope = ropes.get(first - 1);
+                Rope secondRope = ropes.get(second - 1);
                 String firstStr = firstRope.getString(firstRope.getRoot());
                 String secondStr = secondRope.getString(secondRope.getRoot());
-                firstStr = firstStr.substring(0, firstStr.length()-1);
-                secondStr = secondStr.substring(0, secondStr.length()-1);
+                firstStr = firstStr.substring(0, firstStr.length() - 1);
+                secondStr = secondStr.substring(0, secondStr.length() - 1);
                 String result = firstStr.concat(secondStr);
                 Rope resultRope = new Rope(result);
-                ropes.add(first-1, resultRope);
+                ropes.add(first - 1, resultRope);
                 ropes.remove(firstRope);
                 ropes.remove(secondRope);
-            }
-            else if (check.equalsIgnoreCase("exit")) {
-                break;
+            } else if (check.equalsIgnoreCase("index")) {
+                Rope newRope = new Rope("I_ am_ rope_ data_ structure");
+                Scanner scanner = new Scanner(System.in);
+//                int num = Integer.parseInt(st.nextToken());
+                int ind = scanner.nextInt();
+                System.out.println(newRope.index(newRope, ind));
+            } else if (check.equalsIgnoreCase("exit")) {
+                System.exit(0);
             }
         }
     }
--- src/Rope.java
+++ src/Rope.java
@@ -3,6 +3,7 @@ import java.util.StringTokenizer;
 public class Rope {
     private Node root;
     private String out = "";
+
     Rope(String input) {
         root = new Node(null, 0);
         newRope(input, root);
@@ -29,26 +30,24 @@ public class Rope {
             int rightChild = st.countTokens() - leftChild;
             String pass = st.nextToken();
             int length = pass.length();
-            for (int i=1; i<leftChild; i++) {
+            for (int i = 1; i < leftChild; i++) {
                 String temp = st.nextToken();
                 length += temp.length();
-                pass = pass.concat(" "+temp);
+                pass = pass.concat(" " + temp);
             }
             newRope(pass, root.getLeft());
             root.setN(length);
             pass = st.nextToken();
-            for (int i=1; i<rightChild; i++) {
-                pass = pass.concat(" "+st.nextToken());
+            for (int i = 1; i < rightChild; i++) {
+                pass = pass.concat(" " + st.nextToken());
             }
             newRope(pass, root.getRight());
-        }
-        else {
-            if (st.countTokens()==1) {
+        } else {
+            if (st.countTokens() == 1) {
                 String rightStr = st.nextToken();
                 root.setN(rightStr.length());
                 root.setStr(rightStr);
-            }
-            else if (st.countTokens() == 2) {
+            } else if (st.countTokens() == 2) {
                 String leftStr = st.nextToken();
                 root.setN(leftStr.length());
                 Node left = new Node(leftStr, leftStr.length());
@@ -58,6 +57,52 @@ public class Rope {
                 root.setRight(right);
             }
         }
+    }
+
+    public char index(Rope rope, int i) {
+        Node tmp = rope.root;
+        if (i <= tmp.getN()) {
+            while (tmp.getStr() == null && i < tmp.getN())
+                tmp = tmp.getLeft();
+            if (tmp.getRight() == null && tmp.getLeft() == null)
+                return tmp.getStr().charAt(i);
+
+            if (tmp.getRight() != null) {
+                while (tmp.getRight().getStr() == null && i > tmp.getN()) {
+                    tmp = tmp.getRight();
+                }
+            }
+
+            if (i < tmp.getN() && tmp.getStr() != null) {
+                return tmp.getLeft().getStr().charAt(i);
+            } else {
+                return tmp.getRight().getStr().charAt(i - tmp.getN());
+            }
+
+        } else {
+
+            while (tmp.getStr() == null && i >= tmp.getN()) {
+                i -= tmp.getN();
+                tmp = tmp.getRight();
+            }
+
+            if (tmp.getRight() == null && tmp.getLeft() == null)
+                return tmp.getStr().charAt(i);
+
+
+            if (tmp.getLeft() != null) {
+                while (tmp.getLeft().getStr() == null && i <= tmp.getN()) {
+                    tmp = tmp.getLeft();
+                }
+
+                if (i < tmp.getN() && tmp.getLeft().getStr() != null)
+                    return tmp.getLeft().getStr().charAt(i);
+
+            } else {
+                return tmp.getRight().getStr().charAt(i - tmp.getN());
+            }
+        }
+        return '-';
     }
 
     public Node getRoot() {
@@ -70,7 +115,7 @@ public class Rope {
         }
         getString(node.getLeft());
         if (node.getStr() != null) {
-            out = out.concat(node.getStr()+" ");
+            out = out.concat(node.getStr() + " ");
         }
         getString(node.getRight());
         return out;
